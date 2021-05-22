d55ee13abdc4b93ef7c4f6528f986e3a03bcd877
Eric Feng <ericfeng610@gmail.com>
2021-05-22 16:44:10 -0700

    test linting again again

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -2,7 +2,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (_req: NextApiRequest, res: NextApiResponse): void => {
-    res.status(200).json({ name: 'Hello World!!' })
-        console.log('hi')
-    return
+    res.status(200).json({ name: "Hello World!!" });
+    console.log("hi");
+    return;
 };
