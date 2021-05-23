59c42a50d9cbb39510d2f38df0f28d7637af3075
Eric Feng <ericfeng610@gmail.com>
2021-05-23 12:11:46 -0700

    changed matching files for linting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,12 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@definitions/chakra/theme";
 import "@styles/global.css";
-import { Provider } from "react-redux";
-import store from "@redux/store";
 import { appWithTranslation } from "@i18n";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <ChakraProvider theme={theme}>
-            <Provider store={store}>
-                <Component {...pageProps} />
-            </Provider>
+            <Component {...pageProps} />
         </ChakraProvider>
     );
 }
