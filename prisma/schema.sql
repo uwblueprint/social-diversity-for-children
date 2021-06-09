9439b15959992db5c5fcf80478f13d1270c80b25
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-06-09 18:48:58 -0400

    Set up New Database Workflow and Introspect Database Schema

---
+++ prisma/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "VerificationRequest" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+CREATE UNIQUE INDEX "VerificationRequest.token_unique" ON "VerificationRequest"("token");
+
+CREATE UNIQUE INDEX "VerificationRequest.identifier_token_unique" ON "VerificationRequest"("identifier", "token");
