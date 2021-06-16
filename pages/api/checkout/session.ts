28f00525f05e5f7ad12cf1341c19e63d2dbbca67
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-06-15 22:08:37 -0400

    Add Documentation To Session Endpoint

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -1,23 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
+// set up stripe instance
+// TODO: set this as exportable variable in utils directory
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2020-08-27",
 });
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { quantity } = req.body;
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [
-            {
-                price: process.env.PRICE_ID,
-                quantity: quantity,
-            },
-        ],
-        mode: "payment",
-        success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/checkout`,
-    });
-    res.status(200).json({ sessionId: session.id });
-};
+/**
+ * sessionHandler creates a new stripe session and returns the
+ * session ID
+ * @param req API request object
+ * @param res API response object
+ */
+export default async function sessionHandler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+): Promise<void> {
+    if (req.method == "POST") {
+        const { quantity } = req.body;
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price: process.env.PRICE_ID,
+                    quantity: quantity,
+                },
+            ],
+            mode: "payment",
+            success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${req.headers.origin}/checkout`,
+        });
+        res.status(200).json({ sessionId: session.id });
+    } else {
+        // TODO: add JSON response for method not allowed
+        res.status(405);
+    }
+}
