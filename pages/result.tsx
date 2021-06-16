d22ced2303fc83ac17aca401ffd170f797d9a0b5
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-06-15 22:15:25 -0400

    Add Page For Post-Checkout

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,14 +1,20 @@
 import { useRouter } from "next/router";
-import session from "./api/checkout/session";
+import useSWR from "swr";
 
 export default function Result() {
     const router = useRouter();
-    const { session_id } = router.query;
+
+    const { data, error } = useSWR(
+        router.query.session_id
+            ? `/api/checkout/${router.query.session_id}`
+            : null,
+        (url) => fetch(url).then((res) => res.json()),
+    );
 
     return (
         <div>
-            <h1>Payment Success</h1>
-            <pre>{session_id}</pre>
+            <h1>Payment Result</h1>
+            <pre>{data ? JSON.stringify(data, null, 2) : "Loading..."}</pre>
         </div>
     );
 }
