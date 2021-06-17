9fbe84f21bebc1a30ce8c556ab993d146c5e374d
brandonwong368 <brandon.wong368@gmail.com>
2021-06-17 19:31:15 -0400

    Update env variables in index.js to match staging

--- aws/index.js
+++ aws/index.js
@@ -2,8 +2,8 @@ import aws from "aws-sdk";
 
 // Update AWS Config
 aws.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
     signatureVersion: "v4",
 });
