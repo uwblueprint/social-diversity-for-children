4156ac1af5483819aca96900b2314dc72f6a486e
Jason Huang <jason17huang@gmail.com>
2021-06-16 20:48:21 -0400

    Move aws config to index.js

---
+++ aws/index.js
@@ -0,0 +1,10 @@
+import aws from "aws-sdk";
+
+aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region: process.env.AWS_REGION,
+    signatureVersion: "v4",
+});
+
+export { aws };
--- utils/aws/s3.ts
+++ utils/aws/s3.ts
@@ -1,41 +1,29 @@
 import aws from "aws-sdk";
+import { ManagedUpload } from "aws-sdk/clients/s3";
 import fs from "fs";
 import path from "path";
 
 const S3 = new aws.S3();
 
-/* 
+/**
+ * Uploads a file to the AWS S3 bucket corresponding to env.AWS_BUCKET_NAME
+ * @param {string} filePath - Path of the file to upload
+ * @returns {object} - S3 upload response data
+ */
 
-Uploads a file to the AWS S3 bucket corresponding to AWS_BUCKET_NAME in .env.
-    * filePath: the path of the file to upload
-    * returns: upload response if successful, error object otherwise
-
-Sample usage:
-    myFile = "~/documents/my_file.txt"
-    res = uploadToS3(myFile)
-
-*/
-
-const uploadToS3 = (filePath: string): void => {
-    aws.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-        region: process.env.AWS_REGION,
-        signatureVersion: "v4",
-    });
-
+const uploadToS3 = (filePath: string): ManagedUpload => {
     const fileStream = fs.createReadStream(filePath);
     const uploadParams = {
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: path.basename(filePath),
         Body: fileStream,
     };
-    S3.upload(uploadParams, function (err, data) {
-        if (err) {
-            return err;
-        }
-        return data;
+
+    const res = S3.upload(uploadParams, function (err, data) {
+        console.log(err, data);
     });
+
+    return res;
 };
 
-export { S3, uploadToS3 };
+export { uploadToS3 };
