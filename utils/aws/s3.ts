8798d6abefc54c21c27187d32cbade845b641d21
Jason Huang <jason17huang@gmail.com>
2021-06-17 23:43:05 -0400

    Return the appropriate response object from upload

--- utils/aws/s3.ts
+++ utils/aws/s3.ts
@@ -1,4 +1,3 @@
-import { ManagedUpload } from "aws-sdk/clients/s3";
 import { s3 } from "aws/index";
 import fs from "fs";
 import path from "path";
@@ -7,7 +6,7 @@ import path from "path";
  * Uploads a file to the AWS S3 bucket corresponding to env.AWS_BUCKET_NAME
  * @param {string} bucketName - Name of the bucket to upload the file to
  * @param {string} filePath - Path of the file to upload
- * @returns {object} - S3 upload response data
+ * @returns {promise} - S3 upload response object on resolve, error object on reject
  */
 const uploadToS3 = (bucketName: string, filePath: string): Promise<any> => {
     const fileStream = fs.createReadStream(filePath);
@@ -16,8 +15,15 @@ const uploadToS3 = (bucketName: string, filePath: string): Promise<any> => {
         Key: path.basename(filePath),
         Body: fileStream,
     };
-    const s3upload = s3.upload(uploadParams).promise();
-    return s3upload;
+    return s3
+        .upload(uploadParams)
+        .promise()
+        .then((data) => {
+            return data;
+        })
+        .catch((err) => {
+            return err;
+        });
 };
 
 export { uploadToS3 };
