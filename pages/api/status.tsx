eda579499a73bac87a19b2844b831c402c02ea59
Eric Feng <ericfeng610@gmail.com>
2021-06-19 18:25:01 -0700

    Test to check if deploy by PR works

--- pages/api/status.tsx
+++ pages/api/status.tsx
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 // Creates status endpoint to check if server is responsive
 export default (_req: NextApiRequest, res: NextApiResponse): void => {
+    console.log("Testing if deploy by PR works");
     res.status(200).json({ status: "Healthy" });
     return;
 };
