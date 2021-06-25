087ad2053ad2958ab76a959f74830be8ac1da404
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-06-25 18:58:24 -0400

    Modify Upload Function To Return a Promise

--- services/aws/s3.ts
+++ services/aws/s3.ts
@@ -1,29 +1,44 @@
 import { s3 } from "services/aws/index";
 import fs from "fs";
 import path from "path";
+import { ManagedUpload } from "aws-sdk/clients/s3";
 
 /**
- * Uploads a file to an AWS S3 bucket
- * @param {string} bucketName - Name of the bucket to upload the file to
- * @param {string} filePath - Path of the file to upload
- * @returns {promise} - S3 upload response object on resolve, error object on reject
+ * Uploads a file to an AWS S3 Bucket
+ * @param {string} bucketName Name of the bucket to upload the file to
+ * @param {string} filePath Path of the file to upload
+ * @returns {Promise<ManagedUpload.SendData | Error>} A promise which can be resolved to obtain the S3 Upload
+ * Information and can catch any errors in making the upload call
  */
-const uploadToS3 = (bucketName: string, filePath: string): unknown => {
-    const fileStream = fs.createReadStream(filePath);
-    const uploadParams = {
-        Bucket: bucketName,
-        Key: path.basename(filePath),
-        Body: fileStream,
-    };
-    return s3
-        .upload(uploadParams)
-        .promise()
-        .then((data) => {
-            return data;
-        })
-        .catch((err) => {
-            return err;
-        });
+const uploadToS3 = (
+    bucketName: string,
+    filePath: string,
+): Promise<ManagedUpload.SendData | Error> => {
+    return new Promise((resolve, reject) => {
+        // create file stream from file path
+        const fileStream = fs.createReadStream(filePath);
+
+        // set up upload parameters
+        const uploadParams = {
+            Bucket: bucketName,
+            Key: path.basename(filePath),
+            Body: fileStream,
+        };
+
+        // upload file
+        s3.upload(
+            uploadParams,
+            function (err: Error, data: ManagedUpload.SendData) {
+                // reject errors if there are any
+                if (err) {
+                    return reject(err);
+                }
+
+                // resolve the data object if there are no errors
+                return resolve(data);
+            },
+        );
+    });
 };
 
 export { uploadToS3 };
