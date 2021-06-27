c750f0ce018abc5ab5edf8d6deafbce88270139c
Eric Feng <ericfeng610@gmail.com>
2021-06-27 15:18:32 -0700

    Added fix to prevent multiple instances of Prisma Client in development due to hot reloading

--- services/database/index.ts
+++ services/database/index.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+interface CustomGlobal extends NodeJS.Global {
+    prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") {
+    global.prisma = prisma;
+}
 
 export default prisma;
