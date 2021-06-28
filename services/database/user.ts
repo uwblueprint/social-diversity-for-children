49c6d3a31db96a7761964357320febdbc5669ecd
Jason Huang <jason17huang@gmail.com>
2021-06-27 20:34:53 -0400

    Implement create user endpoints

--- services/database/user.ts
+++ services/database/user.ts
@@ -1,4 +1,9 @@
 import prisma from "@database";
+import { Parent, ProgramAdmin, Teacher, Volunteer } from "@prisma/client";
+import { CreateParentInput } from "models/parent";
+import { CreateProgramAdminInput } from "models/programadmin";
+import { CreateTeacherInput } from "models/teacher";
+import { CreateVolunteerInput } from "models/volunteer";
 
 /**
  * NOTE: https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types
@@ -35,17 +40,100 @@ async function getUsers() {
     return users;
 }
 
+/* 
+TODO (6/27/21): https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#create 
+doc does not specify what happens if the create operation fails - does that mean it should always
+succeed? in that case, do we need to return anything or do we just return response 200 every time?
+*/
+
 /**
  * Creates a new user corresponding to newUserData and returns the newly created user
- * @param newUserData - data corresponding to the new user
- * @returns - the newly created user
+ * @param newTeacherData - data corresponding to the new teacher user
+ * @returns - the newly created teacher user
  */
-async function createUser(newUserData) {
-    const user = await prisma.user.create({
-        // TODO: are created_at and updated_at passed in or auto generated?
-        data: newUserData,
+
+async function createTeacher(
+    newTeacherData: CreateTeacherInput,
+    userId: number,
+): Promise<Teacher> {
+    const teacher = await prisma.teacher.create({
+        data: {
+            ...newTeacherData,
+            users: {
+                connect: { id: userId },
+            },
+        },
     });
-    return user;
+    return teacher;
 }
 
-export { getUser, getUsers, createUser };
+/**
+ * Creates a new user corresponding to newUserData and returns the newly created user
+ * @param newVolunteerData - data corresponding to the new volunteer user
+ * @returns - the newly created volunteer user
+ */
+async function createVolunteer(
+    newVolunteerData: CreateVolunteerInput,
+    userId: number,
+): Promise<Volunteer> {
+    const volunteer = await prisma.volunteer.create({
+        data: {
+            ...newVolunteerData,
+            users: {
+                connect: { id: userId },
+            },
+        },
+    });
+    return volunteer;
+}
+
+/**
+ * Creates a new user corresponding to newUserData and returns the newly created user
+ * @param newParentData - data corresponding to the new user
+ * @returns - the newly created user
+ */
+
+async function createParent(
+    newParentData: CreateParentInput,
+    userId: number,
+): Promise<Parent> {
+    const parent = await prisma.parent.create({
+        data: {
+            ...newParentData,
+            users: {
+                connect: { id: userId },
+            },
+        },
+    });
+    return parent;
+}
+
+/**
+ * Creates a new user corresponding to newUserData and returns the newly created user
+ * @param newProgramAdminData - data corresponding to the new program admin user
+ * @returns - the newly created program admin user
+ */
+
+async function createProgramAdmin(
+    newProgramAdminData: CreateProgramAdminInput,
+    userId: number,
+): Promise<ProgramAdmin> {
+    const programAdmin = await prisma.programAdmin.create({
+        data: {
+            ...newProgramAdminData,
+            users: {
+                connect: { id: userId },
+            },
+        },
+    });
+    return programAdmin;
+}
+
+export {
+    getUser,
+    getUsers,
+    createParent,
+    createProgramAdmin,
+    createTeacher,
+    createVolunteer,
+};
