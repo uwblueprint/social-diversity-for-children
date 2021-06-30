4bdf9641687c6994e2e35decbf5c7d0a1ef5a8d9
Eric Feng <ericfeng610@gmail.com>
2021-06-29 19:32:43 -0700

    Fixed validateCreateProgram

--- utils/validation/program.ts
+++ utils/validation/program.ts
@@ -9,26 +9,13 @@ export function validateCreateProgram(
     newProgramData: createProgramInput,
 ): string[] {
     const validationError = [];
-    const start_date = new Date(newProgramData.start_date);
-    const end_date = new Date(newProgramData.end_date);
+    const start_date = new Date(newProgramData.startDate);
+    const end_date = new Date(newProgramData.endDate);
     if (newProgramData.price < 0) {
         validationError.push("Program price cannot be negative");
     }
     if (start_date > end_date) {
         validationError.push("End date cannot be before the start date");
-    }
-    if (newProgramData.space_total != newProgramData.space_available) {
-        validationError.push(
-            "The space available does not match the total space",
-        );
-    }
-    if (
-        newProgramData.volunteer_space_total !=
-        newProgramData.volunteer_space_available
-    ) {
-        validationError.push(
-            "The volunteer space available does not match the total volunteer space",
-        );
     }
     return validationError;
 }
