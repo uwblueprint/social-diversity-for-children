9388deedc263cd14102729f597f614cb803d1b15
cindy <h687wang@uwaterloo.ca>
2021-06-29 21:40:47 -0400

    add on delete cascade, foreign key id. remove email, rename section to class

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -17,7 +17,7 @@ CREATE TYPE weekdays AS ENUM ('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN');
 -- https://stackoverflow.com/questions/3191664/list-of-all-locales-and-their-short-codes
 -- chinese, english, japanese, korean
 CREATE TYPE locales AS ENUM ('zh', 'en', 'ja', 'ko');
-CREATE TYPE online_formats AS ENUM ('online', 'in-person', 'blended');
+CREATE TYPE program_formats AS ENUM ('online', 'in-person', 'blended');
 -- Create users table
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -41,31 +41,31 @@ CREATE TABLE verification_requests (
 CREATE TABLE programs (
   id SERIAL PRIMARY KEY NOT NULL,
   price INTEGER NOT NULL, -- price in cents, to make it integer
-  online_format online_formats NOT NULL,
+  online_format program_formats NOT NULL,
   tag TEXT NOT NULL, -- art, music, math etc (TODO we might need a tag table later)
 
-  start_date TIMESTAMPTZ NOT NULL, -- earliest start date of all sections
-  end_date TIMESTAMPTZ NOT NULL, -- latest end date of all sections
+  start_date TIMESTAMPTZ NOT NULL, -- earliest start date of all classes
+  end_date TIMESTAMPTZ NOT NULL, -- latest end date of all classes
 
   is_archived BOOLEAN DEFAULT false NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE sections (
+CREATE TABLE classes (
   id SERIAL PRIMARY KEY NOT NULL,
   name TEXT, 
-  age_group TEXT, -- sections are always categorized by age group
+  age_group TEXT, -- classes are always categorized by age group
 
   program_id INTEGER NOT NULL,
-  FOREIGN KEY(program_id) REFERENCES programs(id),
+  FOREIGN KEY(program_id) REFERENCES programs(id) ON DELETE CASCADE,
 
   space_total INTEGER NOT NULL,
   space_available INTEGER NOT NULL,
   volunteer_space_total INTEGER NOT NULL,
   volunteer_space_available INTEGER NOT NULL,
 
-  is_archived BOOLEAN DEFAULT false NOT NULL, -- its possible to archive sections separately
+  is_archived BOOLEAN DEFAULT false NOT NULL, -- its possible to archive classes separately
 
   start_date TIMESTAMPTZ NOT NULL,
   end_date TIMESTAMPTZ NOT NULL,
@@ -83,7 +83,6 @@ CREATE TABLE parents (
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   phone_number VARCHAR(50) NOT NULL,
-  email TEXT UNIQUE NOT NULL,
   is_low_income BOOLEAN DEFAULT false,
   address_line1 TEXT NOT NULL,
   address_line2 TEXT,
@@ -91,15 +90,16 @@ CREATE TABLE parents (
   city_name VARCHAR(50) NOT NULL,
   province provinces NOT NULL,
   preferred_language locales NOT NULL,
+  FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 -- create volunteer table
 CREATE TABLE volunteers (
   id SERIAL PRIMARY KEY NOT NULL,
+  FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  email TEXT UNIQUE,
   phone_number VARCHAR(50),
   is_valid BOOLEAN DEFAULT false,
   background_form_link TEXT,
@@ -115,11 +115,11 @@ CREATE TABLE volunteers (
 -- create volunteer registration table
 CREATE TABLE volunteer_regs (
   volunteer_id INTEGER NOT NULL,
-  section_id INTEGER NOT NULL,
-  FOREIGN KEY(volunteer_id) REFERENCES volunteers(id),
-  FOREIGN KEY (section_id) REFERENCES sections(id) ON DELETE CASCADE,
+  class_id INTEGER NOT NULL,
+  FOREIGN KEY(volunteer_id) REFERENCES volunteers(id) ON DELETE CASCADE,
+  FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE,
   is_valid BOOLEAN DEFAULT false,
-  PRIMARY KEY (volunteer_id, section_id),
+  PRIMARY KEY (volunteer_id, class_id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -134,11 +134,11 @@ CREATE TABLE coupon_users (
 );
 -- create program waitlist table
 CREATE TABLE waitlists(
-  section_id INTEGER NOT NULL,
+  class_id INTEGER NOT NULL,
   parent_id INTEGER NOT NULL,
   FOREIGN KEY (parent_id) REFERENCES parents(id) ON DELETE CASCADE,
-  FOREIGN KEY (section_id) REFERENCES sections(id) ON DELETE CASCADE,
-  PRIMARY KEY (parent_id, section_id),
+  FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE,
+  PRIMARY KEY (parent_id, class_id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -166,12 +166,12 @@ CREATE TABLE parent_of_students(
 CREATE TABLE parent_regs (
   parent_id INTEGER NOT NULL,
   student_id INTEGER NOT NULL,
-  section_id INTEGER NOT NULL,
+  class_id INTEGER NOT NULL,
   is_valid BOOLEAN NOT NULL,
   FOREIGN KEY (parent_id) REFERENCES parents(id) ON DELETE CASCADE,
   FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
-  FOREIGN KEY (section_id) REFERENCES sections(id) ON DELETE CASCADE,
-  PRIMARY KEY (student_id, section_id),
+  FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE,
+  PRIMARY KEY (student_id, class_id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -180,7 +180,7 @@ CREATE TABLE program_admins (
   id SERIAL PRIMARY KEY NOT NULL,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  email TEXT UNIQUE,
+  FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -189,28 +189,28 @@ CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  email TEXT UNIQUE,
+  FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 -- create teacher registration table
 CREATE TABLE teacher_regs (
   teacher_id INTEGER NOT NULL,
-  section_id INTEGER NOT NULL,
+  class_id INTEGER NOT NULL,
   FOREIGN KEY(teacher_id) REFERENCES teachers(id) ON DELETE CASCADE,
-  FOREIGN KEY (section_id) REFERENCES sections(id) ON DELETE CASCADE,
-  PRIMARY KEY (section_id, teacher_id),
+  FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE,
+  PRIMARY KEY (class_id, teacher_id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- create program section translation table
-CREATE TABLE section_translations (
-  section_id INTEGER NOT NULL,
+-- create program class translation table
+CREATE TABLE class_translations (
+  class_id INTEGER NOT NULL,
   name TEXT NOT NULL,
-  description TEXT, -- might be null, in the figma the sections only had a name (ex. singing monkeys)
+  description TEXT, -- might be null, in the figma the classes only had a name (ex. singing monkeys)
   language locales NOT NULL,
-  FOREIGN KEY (section_id) REFERENCES sections(id) ON DELETE CASCADE,
-  PRIMARY KEY (section_id),
+  FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE CASCADE,
+  PRIMARY KEY (class_id),
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
