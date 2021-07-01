4c5a45667661c3a4c645877a258122fccc1629c6
Jason Huang <jason17huang@gmail.com>
2021-07-01 13:09:46 -0400

    Revert getUser arg type to string

--- services/database/user.ts
+++ services/database/user.ts
@@ -9,7 +9,7 @@ import { UserInput } from "models/user";
 /**
  * NOTE: https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types
  * getUser takes the id parameter and returns the user associated with the userId
- * @param {number} id - userId
+ * @param {string} id - userId
  */
 async function getUser(id: string) {
     const user = await prisma.user.findUnique({
