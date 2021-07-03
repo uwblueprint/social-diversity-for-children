0d3984551814cc7cdaef820db72085f7ce35a011
Jason Huang <jason17huang@gmail.com>
2021-07-03 02:45:04 -0400

    Make user input fields optional

--- models/user.ts
+++ models/user.ts
@@ -5,8 +5,8 @@ import type { roles } from "@prisma/client";
 
 export type UserInput = {
     id: string;
-    first_name: string;
-    last_name: string;
-    role: roles;
-    role_data: ParentInput | ProgramAdminInput | VolunteerInput;
+    first_name?: string;
+    last_name?: string;
+    role?: roles;
+    role_data?: ParentInput | ProgramAdminInput | VolunteerInput;
 };
