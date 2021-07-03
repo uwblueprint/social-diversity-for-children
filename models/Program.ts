05d2e5d7a2d27571226667ace9fa0b35a907eae5
victoranyun <vyun@uwaterloo.ca>
2021-07-03 15:57:32 -0400

    Added delete program endpoint along with camel case changes

--- models/Program.ts
+++ models/Program.ts
@@ -1,7 +1,8 @@
 import type { programFormat } from "@prisma/client";
 
 /**
- * Request Body Input for POST /program
+ * custom type used for the POST endpoint when creating a new program
+ * id is not required and is automatically incremented in the record
  */
 export type CreateProgramInput = {
     price: number;
--- services/database/program.ts
+++ services/database/program.ts
@@ -1,6 +1,7 @@
 import prisma from "@database";
 import { CreateProgramInput } from "models/Program";
 import { Program } from "@prisma/client";
+
 /**
  * NOTE: https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types
  * getProgram takes the id parameter and returns the program associated with the programId
