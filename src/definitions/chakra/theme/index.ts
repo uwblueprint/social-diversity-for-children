f527a912979888cd729c8730ee18f56d87d4f050
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-07-04 10:49:48 -0400

    Set Roboto As Default Font For Site UI

--- src/definitions/chakra/theme/index.ts
+++ src/definitions/chakra/theme/index.ts
@@ -1,24 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
-import styles from "./styles";
-
-import colors from "./foundations/colors";
-
-import fontSizes from "./foundations/fontSizes";
-
-/**
- * This file is generated for providing a custom theme to Chakra UI
- *
- * To learn more about custom themes
- * please visit https://chakra-ui.com/docs/getting-started#add-custom-theme-optional
- */
-
-const overrides = {
-    ...styles,
-    colors,
-    fontSizes,
-};
-
-const theme = extendTheme(overrides);
+const theme = extendTheme({
+    fonts: {
+        heading: "Open Sans",
+        body: "Roboto",
+    },
+});
 
 export default theme;
