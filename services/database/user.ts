b3be09cd3681759429466a18bd7befcb02c7135e
Jason Huang <jason17huang@gmail.com>
2021-07-07 22:36:43 -0400

    Fix bugs in validation function and add documentation

--- services/database/user.ts
+++ services/database/user.ts
@@ -1,7 +1,7 @@
 import prisma from "@database";
 import { UserInput, ParentInput, VolunteerInput } from "@models/User";
 import { User, roles } from "@prisma/client";
-import { validateUser } from "@utils/validation/user";
+import { userIsValid } from "@utils/validation/user";
 
 /**
  * NOTE: https://www.prisma.io/docs/concepts/components/prisma-client/advanced-type-safety/operating-against-partial-structures-of-model-types
@@ -51,7 +51,7 @@ async function updateUser(userInput: UserInput): Promise<User> {
     - update the user record
     - return the user, including the role records
     */
-    if (!validateUser(userInput)) {
+    if (!userIsValid(userInput)) {
         return null;
     }
     const roleData = userInput.roleData;
--- utils/validation/user.ts
+++ utils/validation/user.ts
@@ -1,8 +1,13 @@
-import { locale, roles } from ".prisma/client";
+import { locale, roles, province } from ".prisma/client";
 import { UserInput, ParentInput, VolunteerInput } from "models/User";
 import validator from "validator";
 
-function validateProvince(province) {
+/**
+ * Valdaites whether a given province has been specified correctly
+ * @param userProvince - the province to validate
+ * @returns - true if the province is valid, false otherwise
+ */
+function validateProvince(userProvince) {
     return [
         province.AB,
         province.BC,
@@ -17,23 +22,27 @@ function validateProvince(province) {
         province.QC,
         province.SK,
         province.YT,
-    ].includes(province);
+    ].includes(userProvince);
 }
 
-function validatePreferredLanguage(language) {
-    return [locale.en, locale.ja, locale.ko, locale.zh].includes(language);
+/**
+ * Validates whether the specified preferred language has been specified correctly
+ * @param userLanguage - the preferred language to validate
+ * @returns - true if the language is valid, false otherwise
+ */
+function validatePreferredLanguage(userLanguage) {
+    return [locale.en, locale.ja, locale.ko, locale.zh].includes(userLanguage);
 }
 
-function validateUser(user: UserInput): boolean {
+/**
+ * Validates the input for updating a user and its corresponding role
+ * @param user - the user data to validate
+ * @returns - true if the user data is valid, false otherwise
+ */
+function userIsValid(user: UserInput): boolean {
     // validate base user fields
-    const firstNameIsAlpha = validator.isAlpha({
-        str: user.firstName,
-        options: " -",
-    });
-    const lastNameIsAlpha = validator.isAlpha({
-        str: user.lastName,
-        options: " -",
-    });
+    const firstNameIsAlpha = validator.isAlpha(user.firstName, undefined, " -");
+    const lastNameIsAlpha = validator.isAlpha(user.lastName, undefined, " -");
     const roleIsValid = [
         roles.PARENT,
         roles.PROGRAM_ADMIN,
@@ -42,6 +51,7 @@ function validateUser(user: UserInput): boolean {
         null,
     ].includes(user.role);
 
+    // validate the User fields
     const userDataIsValid = firstNameIsAlpha && lastNameIsAlpha && roleIsValid;
 
     // validate role fields
@@ -51,7 +61,10 @@ function validateUser(user: UserInput): boolean {
         const phoneNumberIsValid = validator.isMobilePhone(
             roleData.phoneNumber,
         );
-        const postalCodeIsValid = validator.isPostalCode(roleData.postalCode);
+        const postalCodeIsValid = validator.isPostalCode(
+            roleData.postalCode,
+            "CA",
+        );
         const provinceIsValid = validateProvince(roleData.province);
         const preferredLanguageIsValid = validatePreferredLanguage(
             roleData.preferredLanguage,
@@ -62,20 +75,21 @@ function validateUser(user: UserInput): boolean {
             provinceIsValid &&
             preferredLanguageIsValid;
     } else if (user.role === roles.PROGRAM_ADMIN) {
-        // pass
+        // pass - since program admin has no unique fields
     } else if (user.role === roles.TEACHER) {
-        // pass
+        // pass - since teacher role is not currently supported
     } else if (user.role === roles.VOLUNTEER) {
         const roleData: VolunteerInput = user.roleData;
         const phoneNumberIsValid =
             !roleData.phoneNumber ||
             validator.isMobilePhone(roleData.phoneNumber);
         const postalCodeIsValid =
-            !roleData.postalCode || validator.isPostalCode(roleData.postalCode);
+            !roleData.postalCode ||
+            validator.isPostalCode(roleData.postalCode, "CA");
         const provinceIsValid =
-            roleData.province || validateProvince(roleData.province);
+            !roleData.province || validateProvince(roleData.province);
         const preferredLanguageIsValid =
-            roleData.preferredLanguage ||
+            !roleData.preferredLanguage ||
             validatePreferredLanguage(roleData.preferredLanguage);
         roleDataIsValid =
             phoneNumberIsValid &&
@@ -83,10 +97,12 @@ function validateUser(user: UserInput): boolean {
             provinceIsValid &&
             preferredLanguageIsValid;
     } else {
+        // if the user role does not satisfy one of the above conditions,
+        //  it must be null/undefined - otherwise it's invalid
         roleDataIsValid = !user.role ? true : false;
     }
 
     return userDataIsValid && roleDataIsValid;
 }
 
-export { validateUser };
+export { userIsValid };
