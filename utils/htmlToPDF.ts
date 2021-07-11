690bacc9ea8eda7c4ee364c4cef4b75e06c7ce54
brandonwong368 <brandon.wong368@gmail.com>
2021-07-11 19:41:07 -0400

    Create html to pdf util

---
+++ utils/htmlToPDF.ts
@@ -0,0 +1,19 @@
+import { create, CreateOptions } from "html-pdf";
+import { readFileSync } from "fs";
+/**
+ * converts hard coded html into a PDF and downloads the PDF locally
+ */
+const htmlToPDF = (): void => {
+    // path of the hard coded html or the html as a string
+    const htmlPath = "./test.html";
+
+    const htmlString = readFileSync(htmlPath, "utf8");
+    const options: CreateOptions = { directory: "./", format: "Letter" };
+
+    create(htmlString, options).toFile("./test.pdf", function (err) {
+        if (err) return console.log(err);
+        console.log("PDF downloaded");
+    });
+};
+
+export { htmlToPDF };
