5d74974fa5f6f60dea92e70a108355c9c65f3039
cindy <h687wang@uwaterloo.ca>
2021-07-12 16:29:30 -0400

    try using git checkout staging --  to remove files from pr

--- services/database/index.ts
+++ services/database/index.ts
@@ -1,16 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-interface CustomGlobal extends NodeJS.Global {
-    prisma: PrismaClient;
-}
-
-// Prevent multiple instances of Prisma Client in development
-declare const global: CustomGlobal;
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") {
-    global.prisma = prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
