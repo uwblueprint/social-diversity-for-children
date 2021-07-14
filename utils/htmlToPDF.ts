d085d172d43ca3587eccdb11ec440db4e6b487aa
brandonwong368 <brandon.wong368@gmail.com>
2021-07-13 21:37:10 -0400

    Add form variables

--- utils/htmlToPDF.ts
+++ utils/htmlToPDF.ts
@@ -4,8 +4,17 @@ import { readFileSync } from "fs";
 /**
  * converts hard coded html into a PDF and downloads the PDF locally
  * @param {string} downloadPath download path of the generated pdf
+ * @param {string} volunteerName volunteer name
+ * @param {string} date date that the form will be filled out on, can we just pass in current date?
+ * @param {string} coordinatorName program coordinator name
+ * parameters to be utilised after form html is provided
  */
-const htmlToPDF = (downloadPath: string): void => {
+const htmlToPDF = (
+    downloadPath: string,
+    volunteerName: string,
+    date: Date,
+    coordinatorName,
+): void => {
     // path of the hard coded html or the html as a string
     const htmlPath = "./test.html";
     const htmlString = readFileSync(htmlPath, "utf8");
