e06a75d4f6dc03475e4ca57b4d23ca51c8d64959
Jason Huang <jason17huang@gmail.com>
2021-07-18 23:25:22 -0400

    Clean up footer file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-    Box,
-    Container,
-    Link,
-    SimpleGrid,
-    Stack,
-    Text,
-} from "@chakra-ui/react";
+import { Box, Container, Link, Stack, Text } from "@chakra-ui/react";
 
 const SocialMediaLinks = [
     { name: "Instagram", href: "https://www.instagram.com/sdcfdn/" },
@@ -66,55 +59,6 @@ export const Footer: React.FC = () => {
                         </Link>
                     ))}
                 </Stack>
-            </Container>
-        </Box>
-    );
-};
-
-export const tmpFooter: React.FC = () => {
-    return (
-        <Box bg="#0C53A0" color="white">
-            <Container as={Stack} maxW={"6xl"} py={10}>
-                <SimpleGrid
-                    templateColumns={{
-                        sm: "1fr 1fr",
-                        md: "2fr 1fr 1fr 1fr 1fr",
-                    }}
-                    spacing={8}
-                >
-                    <Stack spacing={6}>
-                        <Text fontSize={"sm"}>
-                            © Social Diversity for Children Foundation 2020
-                        </Text>
-                    </Stack>
-                    {/* <Stack align={"flex-start"}>
-                        <Link href={"#"}>Overview</Link>
-                        <Link href={"#"}>Features</Link>
-                        <Link href={"#"}>Tutorials</Link>
-                        <Link href={"#"}>Pricing</Link>
-                        <Link href={"#"}>Releases</Link>
-                    </Stack>
-                    <Stack align={"flex-start"}>
-                        <Link href={"#"}>About</Link>
-                        <Link href={"#"}>Press</Link>
-                        <Link href={"#"}>Careers</Link>
-                        <Link href={"#"}>Contact</Link>
-                        <Link href={"#"}>Partners</Link>
-                    </Stack>
-                    <Stack align={"flex-start"}>
-                        <Link href={"#"}>Help Center</Link>
-                        <Link href={"#"}>Terms of Service</Link>
-                        <Link href={"#"}>Legal</Link>
-                        <Link href={"#"}>Privacy Policy</Link>
-                        <Link href={"#"}>Status</Link>
-                    </Stack> */}
-                    <Stack align={"flex-start"}>
-                        <Link href={"#"}>Instagram</Link>
-                        <Link href={"#"}>Facebook</Link>
-                        <Link href={"#"}>Twitter</Link>
-                        <Link href={"#"}>YouTube</Link>
-                    </Stack>
-                </SimpleGrid>
             </Container>
         </Box>
     );
