fa6b47b3385cf4d0f8dee11f5ccc4b41fa16919a
cindy <h687wang@uwaterloo.ca>
2021-07-20 21:58:04 -0400

    add a file i forgot to git add

--- models/Class.ts
+++ models/Class.ts
@@ -15,3 +15,15 @@ export type CreateClassInput = {
     startTimeMinutes: number;
     durationMinutes: number;
 };
+
+export type ClassCardInfo = {
+    image?: string;
+    name?: string;
+    ageGroup?: string;
+    spaceAvailable: number;
+    volunteerSpaceAvailable: number;
+    weekday: weekday;
+    startTimeMinutes: number;
+    durationMinutes: number;
+    teacherName: string;
+};
