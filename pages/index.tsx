f4f9f74faff4408af946c3eceeeb44d5d79b3db7
Jason Huang <jason17huang@gmail.com>
2021-07-22 18:52:25 -0400

    Add wrapper component with navbar and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,19 @@
 import { useSession, signIn, signOut } from "next-auth/client";
-import { Navbar } from "@components/Navbar";
-import { Footer } from "@components/Footer";
+import Wrapper from "@components/SDCWrapper";
 
 export default function Component() {
     const [session, loading] = useSession();
     if (session) {
         return (
-            <>
-                <Navbar session={session} />
+            <Wrapper>
                 Signed in as {session.user.email} <br />
                 <button onClick={() => signOut()}>Sign out</button>
-                <Footer />
-            </>
+            </Wrapper>
         );
     }
     return (
-        <>
-            <Navbar />
+        <Wrapper>
             Not signed in <br />
-            <Footer />
-        </>
+        </Wrapper>
     );
 }
