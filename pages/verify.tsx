6ce88c95c878f5b11b4094f22d0acf84484bf73d
Eric Feng <ericfeng610@gmail.com>
2021-07-23 17:12:38 -0700

    Added high fidelity design for verify page

--- pages/verify.tsx
+++ pages/verify.tsx
@@ -1,33 +1,65 @@
-import { Box, Center, Text, Image } from "@chakra-ui/react";
+import { Box, Center, Text, Image, Link } from "@chakra-ui/react";
 import useLocalStorage from "@utils/useLocalStorage";
+import Wrapper from "@components/SDCWrapper";
 
 export default function Verify(): JSX.Element {
     const [localStorageEmail] = useLocalStorage("sdc-email-verification", "");
+    const logoSrc = "/images/sdc-logo-blue.png";
     return (
-        <Center h="500px">
-            <Box width="700px">
-                <Center>
-                    <Text fontWeight="425" fontSize="lg" mt="100px">
-                        An email has been sent to{" "}
-                        <u>
-                            <b>{localStorageEmail}</b>
-                        </u>
-                        ! <br></br>
-                    </Text>
-                </Center>
-                <Center>
-                    <Text fontWeight="425" fontSize="lg" mt="10px">
-                        There should be a magic link to log in!
-                    </Text>
-                </Center>
-                <Center>
-                    <Image
-                        boxSize="420px"
-                        src="/images/vectors/verify-email.svg"
-                        alt="Email Verification"
-                    ></Image>
-                </Center>
-            </Box>
-        </Center>
+        <Wrapper>
+            <Center h="500px">
+                <Box width="700px">
+                    <Center>
+                        <Box>
+                            <Image
+                                w={150}
+                                py={15}
+                                src={logoSrc}
+                                alt="SDC Logo"
+                            ></Image>
+                        </Box>
+                    </Center>
+                    <Center>
+                        <Text
+                            fontWeight="700"
+                            fontSize="24px"
+                            align="center"
+                            mt="50px"
+                        >
+                            A verification email has been sent.
+                            <br></br>
+                            Check your email!
+                        </Text>
+                    </Center>
+                    <Center>
+                        <Text fontWeight="400" fontSize="16px" mt="40px">
+                            To confirm your email address, click on the link in
+                            the email we sent to{" "}
+                            <u>
+                                <b>{localStorageEmail}</b>
+                            </u>
+                            .<br></br>
+                        </Text>
+                    </Center>
+                    <Center>
+                        <Text
+                            fontWeight="400"
+                            fontSize="14px"
+                            mt="100px"
+                            color="brand.300"
+                        >
+                            Didn’t get an email? Return to the{" "}
+                            <Link
+                                _hover={{ textDecoration: "none" }}
+                                href="/login"
+                            >
+                                <Text as="u">Sign In</Text>
+                            </Link>{" "}
+                            page and re-enter a valid email.
+                        </Text>
+                    </Center>
+                </Box>
+            </Center>
+        </Wrapper>
     );
 }
