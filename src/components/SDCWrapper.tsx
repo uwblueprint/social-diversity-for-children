9465963dac405025699a0959339947daa16c3be8
Jason Huang <jason17huang@gmail.com>
2021-07-24 02:27:35 -0400

    Make footer touch bottom of screen for small content

--- src/components/SDCWrapper.tsx
+++ src/components/SDCWrapper.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useSession } from "next-auth/client";
 import { Navbar } from "./Navbar";
-import { Footer } from "./Footer";
+import { Footer, DEFAULT_FOOTER_HEIGHT } from "./Footer";
+import { Box } from "@chakra-ui/react";
 
 const SDCWrapper: React.FC = (props): JSX.Element => {
     const [session, loading] = useSession();
     return (
-        <div>
+        <Box minHeight={"100vh"} position={"relative"}>
             <Navbar session={session} />
-            {props.children}
+            <Box pb={DEFAULT_FOOTER_HEIGHT}>{props.children}</Box>
             <Footer />
-        </div>
+        </Box>
     );
 };
 
