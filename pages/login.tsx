bfb3ad4030ce19acc6da88567f08c90edee57a61
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2021-07-24 00:02:25 -0400

    Integrate Login Page And Add Relevant Redirections (#47)
    * Integrate Login Page With Next Auth
    
    * Redirect To Login Page When Not Authenticated
    
    * Add Missing Import
    
    * Removed checking for authentication in index.tsx
    
    * Fixed comment
    
    Co-authored-by: Eric Feng <ericfeng610@gmail.com>

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,12 +6,23 @@ import {
     Input,
     FormControl,
 } from "@chakra-ui/react";
+import { GetServerSideProps } from "next"; // Get server side props
+import { getSession, GetSessionOptions, signIn } from "next-auth/client";
+import { useState } from "react";
 
 /**
  * This is the page that a user will use to either login or register
  * to the SDC platform
  */
 export default function Login(): JSX.Element {
+    // hook to hold the user's email
+    const [email, setEmail] = useState("");
+
+    // signInWithEmail sends a login request to the user's email
+    const signInWithEmail = () => {
+        signIn("email", { email });
+    };
+
     return (
         <Center h="500px">
             <Box width="700px">
@@ -32,6 +43,8 @@ export default function Login(): JSX.Element {
                             type="email"
                             placeholder="Email address"
                             mt="40px"
+                            onChange={(e) => setEmail(e.target.value)}
+                            onEnter={signInWithEmail}
                         />
                     </Center>
                 </FormControl>
@@ -43,6 +56,7 @@ export default function Login(): JSX.Element {
                         fontSize="10px"
                         fontWeight="400"
                         mt="20px"
+                        onClick={signInWithEmail}
                     >
                         Continue
                     </Button>
@@ -62,3 +76,27 @@ export default function Login(): JSX.Element {
         </Center>
     );
 }
+
+/**
+ * getServerSideProps runs before this page is rendered to check to see if a
+ * user has already been authenticated.
+ */
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    // obtain the next auth session
+    const session = await getSession(context);
+
+    // if the user is already authenticated redirect them to the home page
+    if (session) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    // if the user is not authenticated - continue to the page as normal
+    return {
+        props: {},
+    };
+};
