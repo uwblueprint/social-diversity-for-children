6a6b898bcb9a9a59652f61b84208ddeb9b5226c4
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-07-27 22:04:17 -0400

    Add Documentation To Parameters

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,13 +1,16 @@
 import { CheckoutButton } from "@components/CheckoutButton";
 
+// test price id corresponding to a product in stripe
+const testPriceId = "price_1J1GuzL97YpjuvTOePyVbsRh";
+
+// test quantity to specify number of products
+const testQuantity = 1;
+
 /**
  * This is a test page to test the Checkout Button Component
  * TODO: delete this page once the checkout button is consumed in
  * a different component or page
  */
-
-const testPriceId = "price_1J1GuzL97YpjuvTOePyVbsRh";
-const testQuantity = 1;
 export default function Checkout(): JSX.Element {
     return (
         <div>
