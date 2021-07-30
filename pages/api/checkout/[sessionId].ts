f46af176dd3a8cdf1488b5cf8c4e97754700be4a
Mahad Zaryab <43658574+mahadzaryab1@users.noreply.github.com>
2021-07-30 11:12:24 -0400

    Setup Stripe Coupon Workflow and Minor Refactors (#55)
    * Refactor To Dynamically Set Price ID From Front End
    
    * Refactor To Use Switch Statements and Move Stripe Instance To Services
    
    * Add Documentation To Parameters
    
    * Add Documentation and Use Props In Checkout Button Component
    
    * Refactor To Use Exported Stripe Service

--- pages/api/checkout/[sessionId].ts
+++ pages/api/checkout/[sessionId].ts
@@ -1,11 +1,6 @@
+import { ResponseUtil } from "@utils/responseUtil";
 import { NextApiRequest, NextApiResponse } from "next";
-import Stripe from "stripe";
-
-// set up stripe instance
-// TODO: set this as exportable variable in utils directory
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2020-08-27",
-});
+import { stripe } from "services/stripe";
 
 /**
  * sessionIdHandler takes the sessionId parameter and returns
@@ -17,23 +12,30 @@ export default async function sessionIdHandler(
     req: NextApiRequest,
     res: NextApiResponse,
 ): Promise<void> {
-    if (req.method == "GET") {
-        // obtain the sessionId
-        const { sessionId } = req.query;
+    switch (req.method) {
+        case "GET": {
+            // obtain the sessionId
+            const { sessionId } = req.query;
 
-        // obtain the session information from sessionId
-        const session = await stripe.checkout.sessions.retrieve(
-            sessionId as string,
-            {
-                expand: ["payment_intent"],
-            },
-        );
+            // obtain the session information from sessionId
+            const session = await stripe.checkout.sessions.retrieve(
+                sessionId as string,
+                {
+                    expand: ["payment_intent"],
+                },
+            );
 
-        // return session information
-        res.status(200).json({ session });
-    } else {
-        res.setHeader("Allow", ["GET"]);
-        // TODO: add JSON response for method not allowed
-        res.status(405);
+            // return session information
+            res.status(200).json({ session });
+            break;
+        }
+        default: {
+            const allowedHeaders: string[] = ["GET"];
+            ResponseUtil.returnMethodNotAllowed(
+                res,
+                allowedHeaders,
+                `Method ${req.method} Not Allowed`,
+            );
+        }
     }
 }
