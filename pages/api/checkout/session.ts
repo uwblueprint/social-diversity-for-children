ace9635c9a9eef3875d673f7ae9793e412e810c5
Mahad Zaryab <mahadzaryab1@gmail.com>
2021-08-02 11:42:45 -0400

    Add Functionality For Automatic Discounts

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -16,21 +16,46 @@ export default async function sessionHandler(
     switch (req.method) {
         case "POST": {
             const stripeRequest = req.body as StripeCheckoutRequest;
-            const session = await stripe.checkout.sessions.create({
-                payment_method_types: ["card"],
-                line_items: [
-                    {
-                        price: stripeRequest.priceId,
-                        quantity: stripeRequest.quantity,
-                    },
-                ],
-                mode: "payment",
-                success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-                cancel_url: `${req.headers.origin}/checkout`,
-                allow_promotion_codes: true,
-            });
+            // if a coupon ID is passed in then apply an automatic discount
+            if (stripeRequest.couponId) {
+                const session = await stripe.checkout.sessions.create({
+                    payment_method_types: ["card"],
+                    line_items: [
+                        {
+                            price: stripeRequest.priceId,
+                            quantity: stripeRequest.quantity,
+                        },
+                    ],
+                    mode: "payment",
+                    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+                    cancel_url: `${req.headers.origin}/checkout`,
+                    discounts: [
+                        {
+                            coupon: stripeRequest.couponId,
+                        },
+                    ],
+                });
 
-            res.status(200).json({ sessionId: session.id });
+                res.status(200).json({ sessionId: session.id });
+            }
+            // otherwise allow promotion codes to be entered
+            else {
+                const session = await stripe.checkout.sessions.create({
+                    payment_method_types: ["card"],
+                    line_items: [
+                        {
+                            price: stripeRequest.priceId,
+                            quantity: stripeRequest.quantity,
+                        },
+                    ],
+                    mode: "payment",
+                    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+                    cancel_url: `${req.headers.origin}/checkout`,
+                    allow_promotion_codes: true,
+                });
+
+                res.status(200).json({ sessionId: session.id });
+            }
             break;
         }
         default: {
