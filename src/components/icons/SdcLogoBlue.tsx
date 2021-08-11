e1dea449a5b091eb3e965facd5f159815fbbba0f
brandonwong368 <brandon.wong368@gmail.com>
2021-08-10 22:12:34 -0400

    Add svg sdc logo and resize

--- src/components/icons/SdcLogoBlue.tsx
+++ src/components/icons/SdcLogoBlue.tsx
@@ -4,8 +4,8 @@ function SvgSdcLogoBlue(props: React.SVGProps<SVGSVGElement>) {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
+            width="198px"
+            height="52px"
             viewBox="0 0 400 118.519"
             {...props}
         >
