843e600bf85078afd504762beb7f341362c9b76b
brandonwong368 <brandon.wong368@gmail.com>
2021-08-10 22:18:32 -0400

    Add comments

--- src/components/icons/SdcLogoBlue.tsx
+++ src/components/icons/SdcLogoBlue.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 function SvgSdcLogoBlue(props: React.SVGProps<SVGSVGElement>) {
+    // refer to figma for logo sizing
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
