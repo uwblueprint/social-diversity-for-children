d083bfdd6f6d310e1451079656f3602ee0ba8656
brandonwong368 <brandon.wong368@gmail.com>
2021-08-13 00:48:45 -0400

    Manually update merge conflicts

--- pages/program-details/[pid].tsx
+++ pages/program-details/[pid].tsx
@@ -1,99 +1,54 @@
 import { useRouter } from "next/router";
 import React from "react";
-import { Text } from "@chakra-ui/react";
-import type { ClassCardInfo } from "models/Class";
-import { weekday } from "@prisma/client";
+import { Text, Spinner, Center } from "@chakra-ui/react";
 import { useSession } from "next-auth/client";
 import { ProgramInfo } from "@components/ProgramInfo";
+import useSWR from "swr";
+import CardInfoUtil from "utils/cardInfoUtil";
 
 export const ProgramDetails: React.FC = () => {
     const [session, loading] = useSession();
     const router = useRouter();
     const { pid } = router.query;
-    const programName: string = typeof pid === "string" ? pid : "";
-    // TODO remove test data and get new images
-    const classInfo: { [programId: string]: ClassCardInfo[] } = {
-        "0": [
-            {
-                image: "https://i.pinimg.com/originals/4b/ce/8a/4bce8a95dcd0cab2e2622775835281d4.png",
-                name: "Singing Monkeys",
-                ageGroup: "9 & Under",
-                spaceAvailable: 5,
-                volunteerSpaceAvailable: 4,
-                weekday: weekday.WED,
-                startTimeMinutes: 1080,
-                durationMinutes: 60,
-                teacherName: "Brian",
-            },
-            {
-                image: "https://image.flaticon.com/icons/png/512/2395/2395826.png",
-                name: "Singing Giraffes",
-                ageGroup: "9 & Under",
-                spaceAvailable: 5,
-                volunteerSpaceAvailable: 4,
-                weekday: weekday.THU,
-                startTimeMinutes: 1110,
-                durationMinutes: 60,
-                teacherName: "Brian",
-            },
-        ],
-        "1": [
-            {
-                image: "https://media.giphy.com/media/jEyKIvmt0BgLC/giphy.gif",
-                name: "Class 1",
-                ageGroup: "10 & Under",
-                spaceAvailable: 2,
-                volunteerSpaceAvailable: 4,
-                weekday: weekday.MON,
-                startTimeMinutes: 1234,
-                durationMinutes: 60,
-                teacherName: "Teacher1",
-            },
-            {
-                image: "https://i.pinimg.com/originals/fb/f7/64/fbf76458d74b5236cb7ee9208563bebc.gif",
-                name: "Class 2",
-                ageGroup: "10 & Under",
-                spaceAvailable: 1,
-                volunteerSpaceAvailable: 4,
-                weekday: weekday.THU,
-                startTimeMinutes: 765,
-                durationMinutes: 60,
-                teacherName: "Teacher2",
-            },
-        ],
-    };
-    // TODO this will be a ProgramCardInfo (once the reverted PR gets unreverted)
-    const programInfo: { [programId: string]: any } = {
-        "0": {
-            name: "Building Bridges with Music",
-            description:
-                "Children with special needs will be able to connect with the music teacher through an online video call to socialize and have fun while learning about music!",
-            image: "",
-            startDate: "July 7, 2021",
-            endDate: "August 28, 2021",
-            tag: "Music",
-            format: "Online",
-        },
-        "1": {
-            name: "Education Through Creativity",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            image: "",
-            startDate: "July 7, 2021",
-            endDate: "August 28, 2021",
-            tag: "Art",
-            format: "Online",
-        },
-    };
+
+    const fetchWithId = (url, id) =>
+        fetch(`${url}?id=${id}`).then((r) => r.json());
+    const { data: classListResponse, error: classListError } = useSWR(
+        ["/api/class", pid],
+        fetchWithId,
+    );
+    const { data: programInfoResponse, error: programInfoError } = useSWR(
+        ["/api/program/" + pid, pid],
+        fetchWithId,
+    );
+    if (classListError || programInfoError) {
+        return (
+            <Text>
+                An error has occurred.{" "}
+                {classListError
+                    ? classListError.toString()
+                    : programInfoError.toString()}
+            </Text>
+        );
+    }
+    const programCardInfo = programInfoResponse
+        ? CardInfoUtil.getProgramCardInfo(programInfoResponse.data)
+        : null;
+    const classCardInfos = classListResponse
+        ? CardInfoUtil.getClassCardInfos(classListResponse.data)
+        : [];
+
     // if program name in program info, pass in programInfo[programName]
-    return programName in programInfo ? (
+    return programCardInfo && classCardInfos ? (
         <ProgramInfo
-            programInfo={programInfo[programName]}
+            programInfo={programCardInfo}
             session={session}
-            classInfo={classInfo[programName]}
+            classInfo={classCardInfos}
         />
     ) : (
-        <Text>Index {programName} doesn't have test data yet</Text>
+        <Center>
+            <Spinner size="xl" />
+        </Center>
     );
 };
 
