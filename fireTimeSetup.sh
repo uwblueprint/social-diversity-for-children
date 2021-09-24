8c5d27723a8486817c86be18a1216dcf4331ed63
Greg Maxin <gmaxin@uwaterloo.ca>
2021-09-23 20:04:16 -0400

    Update fireTimeSetup.sh
    Co-authored-by: Ri Xin (Rickson) Yang <44826218+MatoPlus@users.noreply.github.com>

--- fireTimeSetup.sh
+++ fireTimeSetup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install nodejs node.js npm
+sudo apt-get install nodejs yarnpkg npm
 
 #heroku
 curl https://cli-assets.heroku.com/install.sh | sh
