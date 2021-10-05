fab126eb2810e5a5dd75959171f9dee16cd38a6d
matthewgeng <matthewgeng2@gmail.com>
2021-10-05 18:55:45 -0400

    add terraform files

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,25 @@
+# should add variable condition checks to limit configuration discrepencies
+variable "s3_bucket_name" {
+  description = "Name of the document uploads s3 bucket"
+  default     = "sdc-uploads"
+  type        = string
+}
+
+variable "criminal_check_folder" {
+  description = "Name of the criminal check folder"
+  default     = "criminal_checks"
+  type        = string
+}
+
+variable "income_proof_folder" {
+  description = "Name of the income proof folder"
+  default     = "criminal_checks"
+  type        = string
+}
+
+variable "curriculum_plans_folder" {
+  description = "Name of the curriculum plans folder"
+  default     = "curriculum_plans"
+  type        = string
+}
+
