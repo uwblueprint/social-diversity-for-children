382a37e2eec45b5000361b2c83311d86451378f3
Brandon Wong <71860803+brandonwong368@users.noreply.github.com>
2021-10-10 19:01:40 -0400

    Bw/media release component (#93)
    * Change to component
    
    * Update media release form
    
    * Remove participant waiver and terms and conditions pages and convert to component
    
    * Remove unused imports
    
    * Remove routers

---
+++ src/components/ParticipantWaiver.tsx
@@ -0,0 +1,81 @@
+import { BackButton } from "@components/BackButton";
+import { Flex, Text, Checkbox, Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { CloseButton } from "@components/CloseButton";
+
+type ParticipantWaiverProps = {
+    styleProps?: Record<string, unknown>;
+};
+
+/**
+ * Participant waiver page within the class registration process
+ * @returns a page component providing SDC's participant waiver and offering an option to accept it
+ */
+export const ParticipantWaiver: React.FC<ParticipantWaiverProps> =
+    (): JSX.Element => {
+        // Next button is disabled by default, activates when a child is selected
+        // Test data to be replaced with children associated with parent during integration
+        const [acceptedTerms, setAcceptedTerms] = useState<boolean>(false);
+
+        return (
+            <>
+                <Flex justifyContent="space-between">
+                    <BackButton />
+                    {/* navigate to browse programs page instead of going back */}
+                    <CloseButton href="/" />
+                </Flex>
+                <Box>
+                    <Text
+                        align="left"
+                        mt="35px"
+                        fontWeight="700"
+                        fontSize="36px"
+                    >
+                        Participant Waiver
+                    </Text>
+                </Box>
+                <Box>
+                    <Text pb="60px" align="left" mt="30px">
+                        I hereby give my full approval and permission for my
+                        son/daughter to attend the Social Diversity for Children
+                        Foundation's programs. I am aware that my child is
+                        expected to respect both the emotional safety and
+                        physical safety of other participants. Parents/Guardians
+                        will be informed if their child's behaviour does not
+                        reflect this standard. Any child who puts the safety of
+                        other participants at risk may be asked to withdraw from
+                        the activity. I agree to hold all Social Diversity for
+                        Children staff, contractors, and volunteers, and the
+                        activity in-charge, and any parties hosting this event,
+                        including officers and directors, from any liability
+                        resulting from the participation of the named
+                        participant in the registered activity.
+                    </Text>
+                </Box>
+                <Box>
+                    <Checkbox
+                        mb="80px"
+                        onChange={() => setAcceptedTerms(!acceptedTerms)}
+                    >
+                        I have read and agree to the terms above
+                    </Checkbox>
+                </Box>
+                {/* Remove for now
+            <Box pb="50px">
+                <Button
+                    height="50px"
+                    width="200px"
+                    borderRadius="6px"
+                    background={!acceptedTerms ? "darkgray" : colourTheme.colors.Blue}
+                    fontWeight="normal"
+                    fontSize="16px"
+                    isDisabled={!acceptedTerms}
+                    color="white"
+                    onClick={() => router.push("/terms-and-conditions")}
+                >
+                    Next
+                </Button>
+            </Box> */}
+            </>
+        );
+    };
---
+++ src/components/TermsAndConditions.tsx
@@ -0,0 +1,99 @@
+import { Flex, Text, Checkbox, Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+type TermsAndConditionsProps = {
+    styleProps?: Record<string, unknown>;
+};
+
+/**
+ * Terms and conditions page within the class registration process
+ * @returns a page component detailing SDC's terms and conditions and offering an option to accept it
+ */
+export const TermsAndConditions: React.FC<TermsAndConditionsProps> =
+    (): JSX.Element => {
+        // Next button is disabled by default, activates when a child is selected
+        // Test data to be replaced with children associated with parent during integration
+        const [acceptedTerms, setAcceptedTerms] = useState<boolean>(false);
+
+        return (
+            <>
+                <Flex justifyContent="space-between"></Flex>
+                <Box>
+                    <Text
+                        align="left"
+                        mt="35px"
+                        fontWeight="700"
+                        fontSize="36px"
+                    >
+                        Terms {"&"} Conditions
+                    </Text>
+                </Box>
+                <Box>
+                    <Text pb="5px" align="left" mt="30px">
+                        All programs will be 8 weeks long (1 session per week)
+                        for a total of $130 ($16.25/session). If any financial
+                        assistance is required, there are subsidies offered by
+                        the government to ensure that children with disabilities
+                        have the opportunity to participate in these programs.
+                        Please checkout our Financial Support document at
+                        sdcprogramming.org for more information.
+                    </Text>
+                </Box>
+                <Box>
+                    <Text pb="5px" align="left" mt="30px">
+                        However, if you are not eligible for any
+                        government-based disability funding, you are eligible
+                        for the SDC Subsidy, which brings the program feed to
+                        $80/8 sessions ($10/session). The SDC subsidy is
+                        provided by the efforts of our youth team, who fundraise
+                        throughout their term.
+                    </Text>
+                </Box>
+                <Box>
+                    <Text pb="5px" align="left" mt="30px">
+                        Applicants will be required to fill out all forms on the
+                        Registration Form to receive the SDC Subsidy. Please
+                        check out our Financial Support document at
+                        sdcprogramming.org for more information on the SDC
+                        Subsidy and the Low-Income Subsidy offered by SDC.
+                        Participants wishing to apply for the low-income subsidy
+                        will be asked to pay the SDC subsidized fee first ($80),
+                        and then will be refunded by SDC later after the
+                        confirmation of eligibility.
+                    </Text>
+                </Box>
+                <Box>
+                    <Text pb="60px" align="left" mt="30px">
+                        Upon registering, the payment will be asked for up-front
+                        in order to ensure the participants' spot in the
+                        program. If after the first session, there are any
+                        uncertainties about attending the program, please refer
+                        to the SDC Program Refund Policy at sdcprogramming.org
+                    </Text>
+                </Box>
+                <Box>
+                    <Checkbox
+                        mb="80px"
+                        onChange={() => setAcceptedTerms(!acceptedTerms)}
+                    >
+                        I have read and agree to the terms above
+                    </Checkbox>
+                </Box>
+                {/* Take out next button for now
+            <Box pb="50px">
+                <Button
+                    height="50px"
+                    width="200px"
+                    borderRadius="6px"
+                    background={!acceptedTerms ? "darkgray" : colourTheme.colors.Blue}
+                    fontWeight="normal"
+                    fontSize="16px"
+                    isDisabled={!acceptedTerms}
+                    color="white"
+                >
+                    Next
+                </Button>
+            </Box> */}
+            </>
+        );
+    };
