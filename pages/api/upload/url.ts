9f61cd1eb5205724d6c9b9b4ab9ead20218a8707
Matthew Geng <35342787+matthewgeng@users.noreply.github.com>
2021-10-10 21:55:51 -0400

    Mg/document upload (#92)
    * update gitignore to include .vscode
    
    * add document upload page and intial components
    
    * add cloud upload image from
    
    * fix spacing issues
    
    * add most recent changes, terraform files, ui + api
    
    * feat(upload): add very rough upload feat
    
    * Add uploading to s3
    
    * add terraform files
    
    * add paths for document uploads, success ui, more robust api
    
    * Remove dynamid path due to vulnerability, update ui
    
    * add session props
    
    * remove png of upload icon
    
    * add comments
    
    * comment out multiple keyword
    
    * add better error control for invalid query parameters
    
    * update env sample
    
    * remove comment, store file size limit in variable
    
    * shorten margin attributes
    
    * add backbutton, add session type, remove complex invalid type check. shorten margin attribute
    
    * remove multiple attribute
    
    * remove terraform files manually, still in commit history will have to change history if we want to completely remove. Files in new branch mg/terraform
    
    * remove terraform gitignore stuff
    
    * change back button to close button in success ui
    
    Co-authored-by: Rickson Yang <rixin.yang.2001@gmail.com>

---
+++ pages/api/upload/url.ts
@@ -0,0 +1,51 @@
+import { s3 } from "@aws";
+import { ResponseUtil } from "@utils/responseUtil";
+import { getSession } from "next-auth/client"; // Session handling
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handle(
+    req: NextApiRequest,
+    res: NextApiResponse,
+): Promise<void> {
+    const session = await getSession({ req });
+    const fileSizeLimitBytes = 5000000; // up to ~5MB
+    // If there is no session or the user is not a parent
+    if (!session) {
+        return ResponseUtil.returnUnauthorized(res, "Unauthorized");
+    }
+
+    // TODO make this more robost/better
+    const accepted_type_paths = [
+        "other",
+        "criminal-check",
+        "income-proof",
+        "curriculum-plans",
+    ];
+
+    if (!accepted_type_paths.includes(req.query.path as string)) {
+        return ResponseUtil.returnNotFound(res, "Type not accepted");
+    }
+
+    switch (req.method) {
+        case "GET": {
+            const post = await s3.createPresignedPost({
+                Bucket: process.env.S3_UPLOAD_BUCKET,
+                Fields: {
+                    key: `${req.query.path}/${req.query.file}`,
+                },
+                Conditions: [["content-length-range", 0, fileSizeLimitBytes]],
+            });
+
+            ResponseUtil.returnOK(res, post);
+            break;
+        }
+        default: {
+            const allowedHeaders: string[] = ["GET"];
+            ResponseUtil.returnMethodNotAllowed(
+                res,
+                allowedHeaders,
+                `Method ${req.method} Not Allowed`,
+            );
+        }
+    }
+}
