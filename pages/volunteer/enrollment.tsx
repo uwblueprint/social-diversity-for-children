6f4c5eb2c5475e3534f7f7422d05d4d7fc20de0f
Brandon Wong <brandon.wong368@gmail.com>
2021-10-12 00:18:42 -0400

    Add submit background check page

--- pages/volunteer/enrollment.tsx
+++ pages/volunteer/enrollment.tsx
@@ -1,27 +1,9 @@
-import { Stack, Button, Box, Center } from "@chakra-ui/react";
+import { Stack, Button } from "@chakra-ui/react";
 import { useState } from "react";
 import colourTheme from "@styles/colours";
-import { EnrollSuccessPage } from "@components/volunteer-enroll/EnrollSuccess";
-
-const FormButton = (props) => {
-    return (
-        <Button
-            bg={colourTheme.colors.Blue}
-            color={"white"}
-            fontWeight="400"
-            onClick={props.onClick}
-            my={8}
-            px={12}
-            borderRadius={100}
-        >
-            {props.children}
-        </Button>
-    );
-};
-
-const FormPage = (props) => {
-    return <Stack spacing={8}>{props.children}</Stack>;
-};
+import { VolunteerEnrolledFormWrapper } from "@components/volunteer-enroll/VolunteerEnrollFormWrapper";
+import { MediaReleaseForm } from "@components/agreement-form/MediaReleaseForm";
+import { SubmitBackgroundCheckForm } from "@components/volunteer-enroll/SubmitBackgroundCheck";
 
 /**
  * This is the page that a volunteer will use to enroll into a class in a program
@@ -31,43 +13,24 @@ export default function VolunteerEnroll({
 }: {
     session: Record<string, unknown>;
 }): JSX.Element {
-    const [pageNum, setPageNum] = useState(0);
-    const formButtonOnClick = () => {
-        setPageNum((prevPage) => prevPage + 1);
+    const [pageNum, setPageNum] = useState<number>(0);
+
+    const nextPage = () => {
+        setPageNum(pageNum + 1);
         window.scrollTo({ top: 0 });
     };
 
-    const formPages = [
-        // Submit a background check
-        <Box>
-            Submit
-            <FormButton onClick={formButtonOnClick}>Next</FormButton>
-        </Box>,
-        // Update a background check
-        <Box>
-            Update
-            <FormButton onClick={formButtonOnClick}>Next</FormButton>
-        </Box>,
-        // media release
-        <Box>
-            Media
-            <FormButton onClick={formButtonOnClick}>Next</FormButton>
-        </Box>,
-        // confirm program registration
-        <Box>
-            Finish
-            <FormButton onClick={formButtonOnClick}>Next</FormButton>
-        </Box>,
+    const pageElements = [
+        <SubmitBackgroundCheckForm onNext={nextPage} />,
+        <MediaReleaseForm onNext={nextPage} />,
     ];
-    const totalPages = formPages.length;
 
     return (
-        <EnrollSuccessPage
+        <VolunteerEnrolledFormWrapper
             session={session}
             pageNum={pageNum}
             setPageNum={setPageNum}
-            totalPages={totalPages}
-            formPages={formPages}
+            formPages={pageElements}
         />
     );
 }
