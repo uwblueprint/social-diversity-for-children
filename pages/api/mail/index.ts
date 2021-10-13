c75ef47c3e58fb71579a8a16080ea0b13cb6961e
Rickson Yang <rixin.yang.2001@gmail.com>
2021-10-12 22:30:37 -0400

    fix(mail): cleanup + date bug fix

--- pages/api/mail/index.ts
+++ pages/api/mail/index.ts
@@ -1,9 +1,10 @@
 import { ResponseUtil } from "@utils/responseUtil";
 import weekdayToString from "@utils/weekdayToString";
-import getHours, { getSimpleDate } from "@utils/timeUtil";
 import type { NextApiRequest, NextApiResponse } from "next";
 import send from "services/nodemailer/mail";
 import findEmails from "@database/mail";
+import convertToShortTimeRange from "@utils/convertToShortTimeRange";
+import { classStartingSoonTemplate } from "@utils/mail/templateUtil";
 
 /**
  * Handles the request to /api/mail (POST)
@@ -70,134 +71,56 @@ function pushMailPromises(
         // looping through parents that are registered to EACH class
         const parentRegs = classes[i].parentRegs;
         const volunteerRegs = classes[i].volunteerRegs;
-        for (let j = 0; j < parentRegs.length; ++j) {
-            const classStartDate = classes[i].startDate;
-            const emailSubject = `Class Reminder: ${
-                classes[i].name
-            } ${weekdayToString(classes[i].weekday)}s ${getHours(
-                classStartDate,
-            ).slice(0, -2)}-${getHours(
-                classStartDate.setHours(classStartDate.getHours() + 1),
-            )}`;
-            // TODO: Zoom link + contact/unregister included in the body
-            const emailBody = `
-            <head>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Poppins"
-                    rel="stylesheet"
-                    />
-                <style>
-                    body {
-                    font-family: "Poppins";
-                    }
-                </style>
-            </head>
-            <body style="background-color: #fff; padding: 30px; position: absolute">
-                <p style="font-size: 30px"><img src=https://images.squarespace-cdn.com/content/5e83092341f99d6d384777ef/1592547010897-WF00319AKLJCVGJZC3ZK/sdc+logo+with+name+alt.png?content-type=image%2Fpng
-                    style="width: 250px; padding-bottom: 10px; color: #0c53a0" alt="SDC Logo"/></p>
-                <span style="font-style: normal;
-                    font-weight: bold;
-                    font-size: 24px;
-                    line-height: 36px;">Your class is starting in ${intervalHours} hours!</span>
-                <p style="font-style: normal;
-                    font-weight: normal;
-                    font-size: 16px;
-                    line-height: 24px;">This is an automatic reminder for the following class:</p>
-                <span
-                    style="
-                    padding-left:100px;width:375px;font-weight:Bold;font-size:16px;
-                    "
-                    >${classes[i].name}</span
-                    ><br><span
-                    style="
-                    padding-left:100px;width:30px;color:rgba(115,115,115,1);font-size:14px;
-                    "
-                    >${weekdayToString(classes[i].weekday)}s ${getHours(
-                classStartDate,
-            ).slice(0, -2)}-${getHours(
-                classStartDate.setHours(classStartDate.getHours() + 1),
-            )} </span> <br> <span style="padding-left:100px;width:30px;color:rgba(115,115,115,1);font-size:14px;"> ${getSimpleDate(
-                classes[i].startDate,
-            )} to ${getSimpleDate(classes[i].endDate)} </span>
-                <div style="width:68px;height:77px;background:url('${
-                    classes[i].imageLink
-                }'); display: flex; background-size:cover;"></div>
-                <p>Regards, Social Diversity for Children</p>
-            </body>
-            `;
+        const {
+            name,
+            imageLink,
+            weekday,
+            startDate,
+            endDate,
+            startTimeMinutes,
+            durationMinutes,
+        } = classes[i];
+        const emailSubject = `Class Reminder: ${name} ${weekdayToString(
+            weekday,
+        )}s ${convertToShortTimeRange(startTimeMinutes, durationMinutes)}`;
+
+        (parentRegs as any[]).forEach((reg) => {
             mailerPromises.push(
                 send(
                     process.env.EMAIL_FROM,
-                    parentRegs[j].parent.user.email,
+                    reg.parent.user.email,
                     emailSubject,
-                    emailBody,
+                    classStartingSoonTemplate(
+                        intervalHours,
+                        imageLink,
+                        name,
+                        weekday,
+                        startDate,
+                        endDate,
+                        startTimeMinutes,
+                        durationMinutes,
+                    ),
                 ),
             );
-        }
-        // looping through volunteers that are registered to EACH class
-        for (let j = 0; j < volunteerRegs.length; ++j) {
-            const classStartDate = classes[i].startDate;
-            const emailSubject = `Class Reminder: ${
-                classes[i].name
-            } ${weekdayToString(classes[i].weekday)}s ${getHours(
-                classStartDate,
-            ).slice(0, -2)}-${getHours(
-                classStartDate.setHours(classStartDate.getHours() + 1),
-            )}`;
-            const emailBody = `
-            <head>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Poppins"
-                    rel="stylesheet"
-                    />
-                <style>
-                    body {
-                    font-family: "Poppins";
-                    }
-                </style>
-            </head>
-            <body style="background-color: #fff; padding: 30px; position: absolute">
-                <p style="font-size: 30px"><img src=https://images.squarespace-cdn.com/content/5e83092341f99d6d384777ef/1592547010897-WF00319AKLJCVGJZC3ZK/sdc+logo+with+name+alt.png?content-type=image%2Fpng
-                    style="width: 250px; padding-bottom: 10px; color: #0c53a0" alt="SDC Logo"/></p>
-                <span style="font-style: normal;
-                    font-weight: bold;
-                    font-size: 24px;
-                    line-height: 36px;">Your class is starting in ${intervalHours} hours!</span>
-                <p style="font-style: normal;
-                    font-weight: normal;
-                    font-size: 16px;
-                    line-height: 24px;">This is an automatic reminder for the following class:</p>
-                <span
-                    style="
-                    padding-left:100px;width:375px;font-weight:Bold;font-size:16px;
-                    "
-                    >${classes[i].name}</span
-                    ><br><span
-                    style="
-                    padding-left:100px;width:30px;color:rgba(115,115,115,1);font-size:14px;
-                    "
-                    >${weekdayToString(classes[i].weekday)}s ${getHours(
-                classStartDate,
-            ).slice(0, -2)}-${getHours(
-                classStartDate.setHours(classStartDate.getHours() + 1),
-            )} </span> <br> <span style="padding-left:100px;width:30px;color:rgba(115,115,115,1);font-size:14px;"> ${getSimpleDate(
-                classes[i].startDate,
-            )} to ${getSimpleDate(classes[i].endDate)} </span>
-                <div style="width:68px;height:77px;background:url('${
-                    classes[i].imageLink
-                }'); display: flex; background-size:cover;"></div>
-                <p>Regards, Social Diversity for Children</p>
-            </body>
-            `;
+        });
+        (volunteerRegs as any[]).forEach((reg) => {
             mailerPromises.push(
                 send(
                     process.env.EMAIL_FROM,
-                    volunteerRegs[j].volunteer.user.email,
+                    reg.volunteer.user.email,
                     emailSubject,
-                    emailBody,
+                    classStartingSoonTemplate(
+                        intervalHours,
+                        imageLink,
+                        name,
+                        weekday,
+                        startDate,
+                        endDate,
+                        startTimeMinutes,
+                        durationMinutes,
+                    ),
                 ),
             );
-        }
+        });
     }
-    return;
 }
