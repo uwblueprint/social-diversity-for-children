9c52752eef36636bb20f843861f98127caf9d95a
Rickson Yang <rixin.yang.2001@gmail.com>
2021-10-13 09:08:47 -0400

    fix(doc-upload): icon usage

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box, Center, Text, Image } from "@chakra-ui/react";
+import { Box, Center, Text } from "@chakra-ui/react";
 import FileUploadButton from "./FileUploadButton";
+import { UploadIcon } from "./icons";
 
 type DrapAndDropProps = {
     setFiles: (files: File[]) => void;
@@ -17,14 +18,12 @@ const DragAndDrop: React.FC<DrapAndDropProps> = ({ setFiles }): JSX.Element => {
             borderColor="gray.500"
         >
             <Center>
-                {/* TODO use svg and local image */}
-                <Image
-                    src="http://www.clker.com/cliparts/S/j/7/o/b/H/cloud-upload-outline.svg.thumb.png"
-                    mt="20px"
-                ></Image>
+                <Box mt="30px">
+                    <UploadIcon />
+                </Box>
             </Center>
             <Center>
-                <Text fontWeight="400" fontSize="18px" mt="18px">
+                <Text fontWeight="bold" fontSize="18px" mt="18px">
                     Drag and drop your file here
                 </Text>
             </Center>
