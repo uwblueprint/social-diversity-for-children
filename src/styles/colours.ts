8e9f72ba987b00c90be7f5c2592c8800292031ee
Brandon Wong <brandon.wong368@gmail.com>
2021-10-15 20:28:50 -0400

    Update colour themes

--- src/styles/colours.ts
+++ src/styles/colours.ts
@@ -1,15 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const colourTheme = extendTheme({
-        colors: {
-                Blue: "#0C53A0",
-                LightBlue: "#2C6AAD",
-                LightGrayBlue: "#E2E8F0",
-                Gray: "#737373",
-                LightGray: "#F8F8F8",
-                DarkGray: "#464646",
-                DarkerGray: "2D3748",
-        },
+    colors: {
+        Blue: "#0C53A0",
+        LightBlue: "#2C6AAD",
+        LightGrayBlue: "#E2E8F0",
+        Gray: "#737373",
+        LightGray: "#F8F8F8",
+        DarkGray: "#464646",
+        DarkerGray: "2D3748",
+    },
 });
 
 export default colourTheme;
