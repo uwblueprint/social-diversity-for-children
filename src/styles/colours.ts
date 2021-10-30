c9c85b83c8223429b79ca76f7a72f9b233dca3c6
Rickson Yang <rixin.yang.2001@gmail.com>
2021-10-30 16:12:50 -0400

    chore(color): better color name

--- src/styles/colours.ts
+++ src/styles/colours.ts
@@ -5,13 +5,14 @@ const colourTheme = extendTheme({
         Blue: "#0C53A0",
         LightBlue: "#2C6AAD",
         Red: "#D8000C",
-        LightGrayBlue: "#E2E8F0",
+        CatskillWhite: "#E2E8F0",
         LightGray: "#F8F8F8",
         Gray: "#737373",
-        MediumGray: "#C5C5C5",
+        PeriwinkleGray: "#DBE5F1",
+        Sliver: "#C5C5C5",
         MildGray: "#B4B4B4",
         DarkGray: "#464646",
-        DarkerGray: "2D3748",
+        OxfordBlue: "#2D3748",
     },
 });
 
