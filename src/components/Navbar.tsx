e0be4c3d585c5a6a6a11a0f2ad20bf383ca35498
Neel Ismail <neelismail01@gmail.com>
2021-11-07 18:36:55 -0500

    creating sidebar for navbar on mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,33 @@ import {
     Box,
     Flex,
     HStack,
+    Icon,
     Link as ChakraLink,
     useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+    Button,
+    Divider,
+    Text,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerContent,
+    DrawerCloseButton,
 } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { MdOutlineClass, MdOutlineDns, MdLogout } from "react-icons/md";
+import { BiUserCircle } from "react-icons/bi";
+import colourTheme from "@styles/colours";
 import { SignInButton } from "@components/SignInButton";
 import { LanguageModal } from "@components/LanguageModal";
 import { ReactNode } from "react";
 import SdcLogoBlue from "@components/icons/SdcLogoBlue";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { Session } from "next-auth";
+import { getSession, signOut } from "next-auth/client";
 
 type NavbarProps = {
     session?: Record<string, unknown>;
@@ -37,17 +55,118 @@ const NavLink = ({ href, children }: { href: string; children: ReactNode }) => (
     </Link>
 );
 
+const SideBar = ({
+    isOpen,
+    onClose,
+    session,
+}: {
+    isOpen: boolean;
+    onClose: () => void;
+    session: Session;
+}) => {
+    const loggedIn = session ? true : false;
+
+    return (
+        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+            <DrawerContent height="100vh">
+                <DrawerCloseButton />
+                <DrawerHeader>
+                    {loggedIn ? (
+                        <Box mt="20">
+                            <Text fontSize="2xl" fontWeight="bold">
+                                Welcome
+                            </Text>
+                            <Text
+                                fontSize="md"
+                                fontWeight="normal"
+                                mt="5"
+                                mb="15"
+                            >
+                                {session.user.email}
+                            </Text>
+                            <Divider mt="15" mb="15" />
+                        </Box>
+                    ) : (
+                        <Box mt="20">
+                            <Text fontSize="md" fontWeight="normal">
+                                Sign in now to register for classes
+                            </Text>
+                            <Button
+                                height="50px"
+                                width="100%"
+                                borderRadius="6px"
+                                background={colourTheme.colors.Blue}
+                                fontWeight="normal"
+                                fontSize="16px"
+                                color="white"
+                                mt="5"
+                                mb="15"
+                            >
+                                Sign In
+                            </Button>
+                            <Divider mt="15" mb="15" color="#cdcdcd" />
+                        </Box>
+                    )}
+                </DrawerHeader>
+                <DrawerBody>
+                    <Flex flexDirection="row" mt="5" mb="5" alignItems="center">
+                        <Icon as={MdOutlineDns} width={6} height={6} />
+                        <Text marginLeft="5">Browse Programs</Text>
+                    </Flex>
+                    <Flex flexDirection="row" mt="5" mb="5" alignItems="center">
+                        <Icon as={MdOutlineClass} width={6} height={6} />
+                        <Text marginLeft="5">View My Classes</Text>
+                    </Flex>
+                    {loggedIn && (
+                        <Flex
+                            flexDirection="row"
+                            mt="5"
+                            mb="5"
+                            alignItems="center"
+                        >
+                            <Icon as={BiUserCircle} width={6} height={6} />
+                            <Text marginLeft="5">Account</Text>
+                        </Flex>
+                    )}
+                </DrawerBody>
+                <DrawerFooter>
+                    {loggedIn && (
+                        <Box w="100%">
+                            <Divider mt="15" mb="15" />
+                            <Flex alignItems="center">
+                                <Icon as={MdLogout} width={6} height={6} />
+                                <Text marginLeft="5" onClick={() => signOut()}>
+                                    Log Out
+                                </Text>
+                            </Flex>
+                        </Box>
+                    )}
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+    );
+};
+
 export const DEFAULT_NAVBAR_HEIGHT = 16;
 
 export const Navbar: React.FC<NavbarProps> = (props) => {
     const router = useRouter();
+    const variant = useBreakpointValue({ base: "sidebar", lg: "navbar" });
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     const accountButton = props.session ? (
         <NavLink href="/myaccounts">My Account</NavLink>
     ) : (
         <SignInButton />
     );
+
     return (
         <>
+            <SideBar
+                isOpen={isOpen}
+                onClose={onClose}
+                session={props.session}
+            />
             <Box bg={"transparent"} color={useColorModeValue("black", "white")}>
                 <Box
                     bg={"transparent"}
@@ -61,7 +180,12 @@ export const Navbar: React.FC<NavbarProps> = (props) => {
                         alignItems={"center"}
                         justifyContent={"space-between"}
                     >
-                        <HStack spacing={8} alignItems={"center"}>
+                        <HStack spacing={4} alignItems={"center"}>
+                            {variant === "sidebar" && (
+                                <Button onClick={onOpen}>
+                                    <HamburgerIcon w={6} h={6} />
+                                </Button>
+                            )}
                             <Box>
                                 <Link href="/">
                                     <ChakraLink _focus={{}}>
@@ -85,7 +209,7 @@ export const Navbar: React.FC<NavbarProps> = (props) => {
                             </HStack>
                         </HStack>
                         <Flex alignItems={"center"}>
-                            {accountButton}
+                            {variant === "navbar" && accountButton}
                             <LanguageModal currentLanguage={router.locale} />
                         </Flex>
                     </Flex>
