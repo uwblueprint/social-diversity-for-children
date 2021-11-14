f2d97da3acd216a8c63161715c418ffb33064686
Rickson Yang <rixin.yang.2001@gmail.com>
2021-11-13 22:31:28 -0500

    chore(types): add session types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import { roles } from "@prisma/client";
+
+declare module "next-auth" {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            /** The user's postal address. */
+            address: string;
+        };
+        id: number;
+        role: roles;
+    }
+}
