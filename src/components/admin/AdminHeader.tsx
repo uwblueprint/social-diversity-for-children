826d51efa36cba1bedd76ca7ead31dfba1598987
Brandon Wong <71860803+brandonwong368@users.noreply.github.com>
2021-11-25 18:20:24 -0500

    Implement admin browse programs page (#131)
    * Add top nav bar for internal dashboard
    
    * Add browse program card
    
    * Add browse program cards
    
    * Add browse classes page
    
    * Add program description card
    
    * Refetch class data
    
    * Change page routes
    
    * Add search filter for programs on name and description
    
    * Remove unused css code and add empty state for filter
    
    * Fix empty white space on index page and have dynamic card sizwe
    
    * Modify empty state
    
    * Change header into a component
    
    * Add optional parameters to AdminHeader
    
    * Update empty state
    
    * Final comments
    
    * fix lint

---
+++ src/components/admin/AdminHeader.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { ReactNode } from "react";
+import {
+    Box,
+    Flex,
+    HStack,
+    Link,
+    Text,
+    Button,
+    Divider,
+    Spacer,
+} from "@chakra-ui/react";
+import { SmallAddIcon } from "@chakra-ui/icons";
+import colourTheme from "@styles/colours";
+
+type HeaderLinks = {
+    name: string;
+    url: string;
+};
+
+type AdminHeaderProps = {
+    styleProps?: Record<string, unknown>;
+    headerLinks?: HeaderLinks[];
+};
+
+const NavLink = ({ href, children }: { href: string; children: ReactNode }) => (
+    <Link href={href}>
+        <HStack>
+            <Button
+                fontSize="16px"
+                backgroundColor="white"
+                fontWeight="400"
+                px={"16px"}
+                py={"8px"}
+                border-radius="6px"
+                border="1px solid #0C53A0"
+                leftIcon={<SmallAddIcon />}
+            >
+                {children}
+            </Button>
+        </HStack>
+    </Link>
+);
+
+/**
+ * Displays the Header for admin
+ */
+export const AdminHeader: React.FC<AdminHeaderProps> = ({
+    headerLinks,
+    children,
+}) => {
+    return (
+        <>
+            <Box bg={"transparent"} px={"50px"} pt={"20px"} mx={"auto"}>
+                <Flex>
+                    <HStack spacing={8}>
+                        <Text
+                            fontSize="22px"
+                            fontWeight="bold"
+                            color={colourTheme.colors.Blue}
+                        >
+                            {children}
+                        </Text>
+                    </HStack>
+                    <Spacer />
+                    <HStack spacing={4}>
+                        {!headerLinks
+                            ? null
+                            : headerLinks.map((linkInfo) => (
+                                  <NavLink
+                                      key={linkInfo.name}
+                                      href={linkInfo.url}
+                                  >
+                                      {linkInfo.name}
+                                  </NavLink>
+                              ))}
+                    </HStack>
+                </Flex>
+            </Box>
+            <Divider orientation="horizontal" my={6} border="2px" />
+        </>
+    );
+};
