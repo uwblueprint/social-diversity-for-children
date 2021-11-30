a7987aace5916a082205ef76d149976f8fdf57ef
matthewgeng <matthewgeng2@gmail.com>
2021-11-30 00:19:25 -0500

    terraform test change revert

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,6 +1,5 @@
 # NOTE
 # Variables without a default are inputted secretly. Generally only define variables with a default for non-secrets
-variable "test" {}
 # ------------------------------------------------------------------
 # app
 variable "sdc_domain" {
