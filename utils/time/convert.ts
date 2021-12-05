f9a04cc5c0c6c65fc73775ecf099ca59452b4ca6
Rickson Yang <rixin.yang.2001@gmail.com>
2021-12-04 20:47:29 -0500

    fix: timezone issues in api

--- utils/time/convert.ts
+++ utils/time/convert.ts
@@ -6,3 +6,12 @@
 export function totalMinutes(date: Date): number {
     return date.getMinutes() + date.getHours() * 60;
 }
+
+/**
+ * totalUTCMinutes gets the total minutes of a time in a date in UTC
+ * @param  {Date} date
+ * @returns number
+ */
+export function totalUTCMinutes(date: Date): number {
+    return date.getMinutes() + date.getHours() * 60;
+}
