a9fa49cd95895c38f1219815b1ec9aaa7887cd90
Neel Ismail <neelismail01@gmail.com>
2021-12-22 18:12:03 -0500

    stashing changes

--- src/components/FileUploadButton.tsx
+++ src/components/FileUploadButton.tsx
@@ -3,9 +3,14 @@ import { Button, Center, FormControl, Input } from "@chakra-ui/react";
 type FileUploadButtonProps = {
     children?: React.ReactNode;
     setFiles: (files: File[]) => void;
+    changeBackground: boolean;
 };
 
-const FileUploadButton: React.FC<FileUploadButtonProps> = ({ children, setFiles }): JSX.Element => {
+const FileUploadButton: React.FC<FileUploadButtonProps> = ({
+    children,
+    setFiles,
+    changeBackground,
+}): JSX.Element => {
     const fileInput = useRef<HTMLInputElement>(null);
     const handleClick = (e: React.FormEvent) => {
         e.preventDefault();
@@ -21,13 +26,13 @@ const FileUploadButton: React.FC<FileUploadButtonProps> = ({ children, setFiles 
         <FormControl>
             <Center>
                 <Button
-                    backgroundColor="#0C53A0"
-                    borderColor="brand.400"
+                    backgroundColor={changeBackground ? "#E2E8F0" : "#0C53A0"}
+                    borderColor={changeBackground ? "#E2E8F0" : "brand.400"}
                     width="366px"
                     height="54px"
                     fontSize="16px"
                     fontWeight="400"
-                    color="white"
+                    color={changeBackground ? "#2D3748" : "white"}
                     border="1px"
                     mt="20px"
                     onClick={handleClick}
